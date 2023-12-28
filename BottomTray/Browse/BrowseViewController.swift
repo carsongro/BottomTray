2f19adc2665b401474843f5fa399a3154fc22d75
Carson Gross <carsongro@gmail.com>
2023-12-28 10:11:09 -0700

    Added files for browse

--- BottomTray/Browse/BrowseViewController.swift
+++ BottomTray/Browse/BrowseViewController.swift
@@ -6,11 +6,26 @@
 //
 
 import UIKit
+import SwiftUI
+import MusicKit
 
 class BrowseViewController: UIViewController {
+    
+    private var swiftUIHomeSearchController: UIHostingController<BrowseSearchView>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        addSwiftUIController()
+    }
+    
+    private func addSwiftUIController() {
+        let searchController = UIHostingController(rootView: BrowseSearchView())
+        addChild(searchController)
+        searchController.didMove(toParent: self)
+        view.addSubview(searchController.view)
+        searchController.view.frame = view.frame
+        
+        swiftUIHomeSearchController = searchController
     }
 }
